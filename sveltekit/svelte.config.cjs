619db02a3f6088616b5dce51aa84280e8df92c2f
Sotaro Tommykawa <tommysouta@gmail.com>
2021-04-19 16:31:02 +0900

    add adapter-static

--- sveltekit/svelte.config.cjs
+++ sveltekit/svelte.config.cjs
@@ -1,4 +1,5 @@
-const node = require('@sveltejs/adapter-node');
+const adapter = require('@sveltejs/adapter-node');
+// const adapter = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +8,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node(),
+		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
